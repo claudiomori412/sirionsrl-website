b72b9d3aabfe63f0ecfc061180eba79839f1b45d
claudiomori <claudio.mori@sirionsrl.com>
2024-07-03 07:33:22 +0000

    fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,60 +1,54 @@
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from "next";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://sirion-website.vercel.app',
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
-    },
-    {
-      url: 'https://sirion-website.vercel.app/company',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
+      url: "https://www.sirionsrl.com/",
+      lastModified: new Date("2024-07-02"),
+      changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-        url: 'https://sirion-website.vercel.app/contact',
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
-      },
-      {
-        url: 'https://sirion-website.vercel.app/job-opportunity',
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
-      },
-      {
-        url: 'https://sirion-website.vercel.app/know-how',
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
-      },
-      {
-        url: 'https://sirion-website.vercel.app/products',
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
-      },
-      {
-        url: 'https://sirion-website.vercel.app/quality',
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
-      },
-      {
-        url: 'https://sirion-website.vercel.app/sostenibility',
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
-      },
-      {
-        url: 'https://sirion-website.vercel.app/technology',
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
-      },
-  ]
+      url: "https://www.sirionsrl.com/company",
+      lastModified: new Date("2024-07-02"),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.sirionsrl.com/contact",
+      lastModified: new Date("2024-07-02"),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.sirionsrl.com/know-how",
+      lastModified: new Date("2024-07-02"),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.sirionsrl.com/products",
+      lastModified: new Date("2024-07-02"),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.sirionsrl.com/sustainability",
+      lastModified: new Date("2024-07-02"),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.sirionsrl.com/technology",
+      lastModified: new Date("2024-07-02"),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.sirionsrl.com/whistleblowing",
+      lastModified: new Date("2024-07-02"),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
 }
